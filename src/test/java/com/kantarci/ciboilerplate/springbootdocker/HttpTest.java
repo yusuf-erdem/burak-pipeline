bc4279608266929e6036f4bf0cfbb6ea35e48a8c
Burak Kantarci <burakkantarci.08@gmail.com>
2021-11-19 11:08:27 +0300

    Update HttpTest.java

--- src/test/java/com/kantarci/ciboilerplate/springbootdocker/HttpTest.java
+++ src/test/java/com/kantarci/ciboilerplate/springbootdocker/HttpTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.assertj.core.api.Assertions.assertThat
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpTest {
