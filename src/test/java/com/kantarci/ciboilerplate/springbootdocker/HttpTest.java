48ca3dfa1a95d9bfcee6d9181d75f11659f382cd
Burak Kantarci <burakkantarci.08@gmail.com>
2022-01-24 18:11:16 +0300

    Update HttpTest.java

--- src/test/java/com/kantarci/ciboilerplate/springbootdocker/HttpTest.java
+++ src/test/java/com/kantarci/ciboilerplate/springbootdocker/HttpTest.java
@@ -27,6 +27,6 @@ public class HttpTest {
 	@Test
 	public void xyz() throws Exception {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-				String.class)).contains("asassaadfsa");
+				String.class)).contains("He");
 	}
 }
